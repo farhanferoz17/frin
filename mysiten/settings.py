16f5f6866660c7fafdf5101576cbfb4a17edc4a7
farhanferoz17 <farhanferoz17@gmail.com>
2019-06-30 14:32:32 +0600

    Fifth Commit

--- mysiten/settings.py
+++ mysiten/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mysiten.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+	'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
